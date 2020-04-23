bb1a7ea50f8117350293cb42526eaa39c75f5138
David Ben-Yaakov <david@tangogolfdigital.com>
2020-04-23 10:37:29 -0500

    Updated dependencies

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,4 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((swift-mode
-  (flycheck-swift-include-search-paths  "/usr/local/lib/merlin/Igis-1.3.0/Igis/.build/debug" "/usr/local/lib/merlin/Scenes-1.0.5/Scenes/.build/debug")))
+  (flycheck-swift-include-search-paths  "/usr/local/lib/merlin/Igis-1.3.1/Igis/.build/debug" "/usr/local/lib/merlin/Scenes-1.0.7/Scenes/.build/debug")))
