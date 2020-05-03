91850850f35889e7b2624d1ffa627fee47b1961b
David Ben-Yaakov <david@tangogolfdigital.com>
2020-05-03 12:20:39 -0500

    Updated for Igis 1.3.4 and Scenes 1.0.10

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,4 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((swift-mode
-  (flycheck-swift-include-search-paths  "/usr/local/lib/merlin/Igis-1.3.1/Igis/.build/debug" "/usr/local/lib/merlin/Scenes-1.0.7/Scenes/.build/debug")))
+  (flycheck-swift-include-search-paths  "/usr/local/lib/merlin/Igis-1.3.4/Igis/.build/debug" "/usr/local/lib/merlin/Scenes-1.0.10/Scenes/.build/debug")))
