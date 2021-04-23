73c4e150ef134d0e8095981dbe00186312a898e2
William Jackson <william.jackson@student.allenisd.org>
2021-04-23 14:54:44 -0500

    updated version number for loading screen

--- Sources/ScenesShell/Background.swift
+++ Sources/ScenesShell/Background.swift
@@ -146,7 +146,7 @@ class Background : RenderableEntity {
             canvas.render(FillStyle(color:Color(red:255, green:255, blue:0)))
             canvas.render(splash)
 
-            let version = Text(location:Point(x:0, y:(canvas.canvasSize!.height)), text:" v0.2.4")
+            let version = Text(location:Point(x:0, y:(canvas.canvasSize!.height)), text:" v0.3.0")
             version.font = "\((canvas.canvasSize!.height/64))pt Arial"
             version.baseline = .bottom
             version.alignment = .left
