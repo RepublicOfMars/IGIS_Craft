695cd0da31dd5b00410ecb610ee138806d357b2b
William Jackson <william.jackson@student.allenisd.org>
2021-05-10 12:18:29 -0500

    ?

--- .dir-locals.el
+++
@@ -1,9 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((swift-mode . 
-  ((lsp-sourcekit-extra-args . (
-	 "-Xlinker" "-L" "-Xlinker" "/usr/local/lib/merlin/Scenes-1.1.5/Scenes/.build/debug" "-Xswiftc" "-I" "-Xswiftc" "/usr/local/lib/merlin/Scenes-1.1.5/Scenes/.build/debug" 
-	 "-Xlinker" "-L" "-Xlinker" "/usr/local/lib/merlin/Igis-1.3.7/Igis/.build/debug" "-Xswiftc" "-I" "-Xswiftc" "/usr/local/lib/merlin/Igis-1.3.7/Igis/.build/debug" 
-
-)))))
