11303fd53490462d2dc3bca9061df915b1439091
William Jackson <william.jackson@student.allenisd.org>
2021-05-10 12:20:05 -0500

    fixed dynamic library error

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((swift-mode . 
+  ((lsp-sourcekit-extra-args . (
+	 "-Xlinker" "-L" "-Xlinker" "/usr/local/lib/merlin/Scenes-1.1.5/Scenes/.build/debug" "-Xswiftc" "-I" "-Xswiftc" "/usr/local/lib/merlin/Scenes-1.1.5/Scenes/.build/debug" 
+
+)))))
